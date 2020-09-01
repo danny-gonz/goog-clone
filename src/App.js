70f0706996bd62ca8bc82ac023ab527d9fc37bdc
danny-gonz <dpg2012@hotmail.com>
2020-09-01 10:27:02 -0700

    added hooks to get functiality going

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import { Switch, Route } from 'react-router-dom';
+import Search from './components/Search';
+import SearchPage from './components/SearchPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route path='/search'>
+          <SearchPage />
+        </Route>
+
+        <Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Route>
+      </Switch>
+
+      {/* Search (results page) */}
     </div>
   );
 }
